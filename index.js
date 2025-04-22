f774a75564a64fa7832f150fe6ef5127ea589286
kazeem Tiamiyu <katiamiyu@gmail.com>
2025-04-22 13:50:39 +0100

    feat: winston logger uncaught and unhandled exception

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+require('express-async-errors');
+const winston = require('winston');
+require('winston-mongodb');
 const debugStartup = require('debug')('app:startup');
 const debugDb = require('debug')('app:database');
 const config = require('config');
@@ -11,12 +14,33 @@ const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
+const error = require('./middleware/errorhandler');
 const express = require('express');
 const server = express();
+const connectionStr = `mongodb+srv://katiamiyu:${config.get('password')}@enlightenmentblog.8tz07.mongodb.net/vidly`;
+
+process.on('unhandledRejection', (ex)=>{
+  throw ex;
+});
+
+winston.add(
+  new winston.transports.File({
+      filename: 'logfile.log',
+      level: 'error'
+    })
+  );
+winston.add(
+  new winston.transports.MongoDB(
+    {
+      db: connectionStr,
+      level: 'info'
+    })
+  );
+winston.exceptions.handle(new winston.transports.File({filename: 'exceptionlogs.log'}));
 
 // database
-mongoose.connect(`mongodb+srv://katiamiyu:${config.get('password')}@enlightenmentblog.8tz07.mongodb.net/vidly`)
-.then(()=>debugDb('connected to database'))
+mongoose.connect(connectionStr)
+.then(()=>winston.info('connected to database'))
 .catch((error)=>debugStartup(error.message));
 
 // middlewares
@@ -32,6 +56,7 @@ server.use('/api/movies', movies);
 server.use('/api/rentals', rentals);
 server.use('/api/users', users);
 server.use('/api/auth', auth);
+server.use(error);
 
 const port = process.env.PORT || 3000;
 server.listen(port, ()=>{
